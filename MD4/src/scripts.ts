ff75e99d4d7b9947728387fbaf7e96a978b0b423
matissoz <matiss.zelmens@icloud.com>
2023-01-08 14:39:50 +0200

    up2

--- MD4/src/scripts.ts
+++ MD4/src/scripts.ts
@@ -803,7 +803,7 @@ const task42 = (a: string[]) => {
 
     let answ = {} as any;
 
-    for (let i =0; i < a.length; i++) {
+    for (let i = 0; i < a.length; i++) {
         let currentWord = a[i];
         let firstChar = currentWord[0].toLowerCase();
         let innerArr = [];
@@ -886,3 +886,440 @@ console.log(task44([1,2,3,4,5,6,7,8,9,10],3));
 console.log(task44([10,9,8,7,6,5,4,3,2,1],5)); 
 console.log(task44([7,2,1,6,3,4,5,8,9,10],2)); 
 
+// TASK45
+// Write a function that takes an object with two properties as argument
+// It should return the value of the property with key country
+
+
+
+type ObjectT45 = Record<string, string>;
+
+
+const task45 = (a: ObjectT45) => {
+
+    return a.country;
+};
+
+console.log('TASK45')
+console.log(task45({ continent: 'Asia',  country: 'Japan'})); 
+console.log(task45({ country: 'Sweden',  continent: 'Europe'})); 
+
+// TASK45
+// Write a function that takes an object with two properties as argument
+// It should return the value of the property with key 'prop-2'
+// Tip: you might want to use the square brackets property accessor
+
+
+
+
+type ObjectT46 = Record<string | number, string | number>;
+
+
+const task46 = (a: ObjectT46) => {
+
+    return a['prop-2'];
+};
+
+console.log('TASK45')
+console.log(task46({  one: 1,  'prop-2': 2})); 
+console.log(task46({  'prop-2': 'two',  prop: 'test'})); 
+
+// TASK47
+// Write a function that takes an object with two properties and a string as arguments
+// It should return the value of the property with key equal to the value of the string
+
+
+type ObjectT47 = Record<string, string>;
+
+
+const task47 = (a: ObjectT45, b:string) => {
+
+    return a[b];
+};
+
+console.log('TASK47')
+console.log(task47({  continent: 'Asia',  country: 'Japan'}, 'continent')); 
+console.log(task47({  country: 'Sweden',  continent: 'Europe'}, 'country'));
+
+
+
+// TASK 48
+// Write a function that takes an object (a) and a string (b) as argument
+// Return true if a has a property with key b
+// Return false otherwise
+
+
+
+
+type ObjectT48 = Record<string, string | number>;
+
+
+const task48 = (a: ObjectT48, b:string) => {
+
+    return b in a;
+};
+
+console.log('TASK48')
+console.log(task48({a:1,b:2,c:3},'b')); 
+console.log(task48({x:'a',y:'b',z:'c'},'a'));
+console.log(task48({x:'a',y:'b',z:'c'},'z'));
+
+
+// TASK49
+// Write a function that a string (a) as argument
+// Create an object that has a property with key 'key' and a value of a
+// Return the object
+
+
+
+type ObjectT49 = {
+    [key: string]: string;
+};
+
+
+const task49 = (b: string) => {
+
+    let a: ObjectT49 = {};
+
+    a['key'] = b;
+
+    return a['key'];
+   
+};
+
+console.log('TASK49')
+console.log(task49('b'));
+console.log(task49('a'));
+console.log(task49('z'));
+
+
+// TASK50
+// Write a function that takes two strings (a and b) as arguments
+// Create an object that has a property with key 'a' and a value of 'b'
+// Return the object
+
+
+type ObjectT50 = {
+    [key: string]: string;
+};
+
+
+const task50 = (a: string, b: string) => {
+
+    let c: ObjectT49 = {};
+
+    c[a] = b;
+
+    return c;
+   
+};
+
+console.log('TASK50')
+console.log(task50('a','b'));
+console.log(task50('z','x'));
+console.log(task50('b','w'));
+
+
+// TASK51
+// Write a function that takes two arrays (a and b) as arguments
+// Create an object that has properties with keys 'a' and corresponding values 'b'
+// Return the object
+
+
+
+type ObjectT51 = {
+    [key: string | number]: string | number;
+};
+
+
+const task51 = (a: any, b: any) => {
+
+    let c: ObjectT51 = {};
+
+    for(let i = 0; i < a.length; i++){
+        c[a[i]] = b[i];
+    }
+
+    return c;
+   
+};
+
+console.log('TASK51')
+console.log(task51(['a','b','c'],[1,2,3]));
+console.log(task51(['w','x','y','z'],[10,9,5,2]));
+console.log(task51([1,'b'],['a',2]));
+
+// TASK52
+// Write a function that takes two arrays (a and b) as arguments
+// Create an object that has properties with keys 'a' and corresponding values 'b'
+// Return the object
+
+
+
+type ObjectT52 = {
+    [key: string | number]: string | number;
+};
+
+
+const task52 = (a52: ObjectT52) => {
+
+    return Object.keys(a52);
+   
+};
+
+console.log('TASK52')
+console.log(task52({a:1,b:2,c:3}));
+console.log(task52({j:9,i:2,x:3,z:4}));
+console.log(task52({w:15,x:22,y:13}));
+
+
+// TASK53
+// Write a function that takes two arrays (a and b) as arguments
+// Create an object that has properties with keys 'a' and corresponding values 'b'
+// Return the object
+
+
+
+type ObjectT53 = {
+    [key: string | number]: number;
+};
+
+
+const task53 = (a: ObjectT52) => {
+
+    let val = 0;
+
+    Object.keys(a).forEach((el) => {
+        val += (a[el] as number);
+    });
+
+    return val;
+}
+
+console.log('TASK53')
+console.log(task53({a:1,b:2,c:3}));
+console.log(task53({j:9,i:2,x:3,z:4}));
+console.log(task53({w:15,x:22,y:13}));
+
+// TASK54
+// Write a function that takes an object as argument
+// It should return an object with all original object properties
+// except for the property with key 'b'
+
+
+
+
+type ObjectT54 = {
+    [key: string | number]: number;
+};
+
+
+const task54 = (a: ObjectT52) => {
+
+    delete a.b;
+
+    return a;
+}
+
+console.log('TASK54')
+console.log(task54({a:1,b:7,c:3}));
+console.log(task54({j:0,b:7,x:3,z:4}));
+console.log(task54({w:15,x:22,b:13}));
+
+// TASK55
+// Write a function that takes two objects as arguments
+// Unfortunately, the property 'b' in the second object has the wrong key
+// should be named 'd' instead
+// Merge both objects and correct the wrong property name
+// Return the resulting object
+// It should have the properties 'a', 'b', 'c', 'd', and 'e'
+
+
+
+type ObjectT55 = Record<string, number>
+
+const task55 = (a: ObjectT55, b: ObjectT55) => {
+    
+    b.d = b.b;
+    delete b.b;
+    
+    return {...a, ...b}
+}
+
+console.log('TASK55')
+console.log(task55({ a: 1, b: 2 }, { c: 3, b: 4, e: 5 }));
+console.log(task55({ a: 5, b: 4 }, { c: 3, b: 1, e: 2 }));
+
+// TASK56
+// Write a function that takes an object (a) and a number (b) as arguments
+// Multiply all values of 'a' by 'b'
+// Return the resulting object
+
+
+
+
+type ObjectT56 = Record<string, number>
+
+const task56 = (a: ObjectT56, b: number) => {
+
+    let key = Object.keys(a);
+    
+    a[key[0]] *= b;
+
+    return a;
+}
+
+console.log('TASK56')
+console.log(task56({a:1,b:2,c:3},3));
+console.log(task56({j:9,i:2,x:3,z:4},10));
+console.log(task56({w:15,x:22,y:13},6));
+
+// TASK57
+// Write a function that takes an object as argument
+// Somehow, the properties and keys of the object got mixed up
+// Swap the Javascript object's key with its values and return the resulting object
+
+
+
+
+
+type ObjectT57 = Record<string | number, number | string>
+
+const task57 = (a: ObjectT57) => {
+
+    const b: ObjectT57 = {};
+    Object.keys(a).forEach(key => {
+      b[a[key]] = key;
+    });
+
+    return b;
+}
+
+console.log('TASK57')
+console.log(task57({z:'a',y:'b',x:'c',w:'d'}));
+console.log(task57({2:'a',4:'b',6:'c',8:'d'}));
+console.log(task57({a:1,z:24}));
+
+
+// TASK58
+// Write a function that takes an object as argument
+// Somehow, the properties and keys of the object got mixed up
+// Swap the Javascript object's key with its values and return the resulting object
+
+
+
+
+
+type ObjectT58 = Record<string | number, number | string>
+
+const task58 = (a: ObjectT58) => {
+
+    Object.keys(a).forEach(key => {
+        if(a[key] === '' || a[key] === ' ')
+        a[key] = null;
+    });
+
+    return a;
+}
+
+console.log('TASK58')
+console.log(task58({ a: 'a', b: 'b', c: '' }));
+console.log(task58({ a: '', b: 'b', c: ' ', d: 'd' }));
+console.log(task58({ a: 'a', b: 'b ', c: ' ', d: '' }));
+
+
+// TASK59
+// Write a function that takes an object as argument containing properties with personal information
+// Extract firstName, lastName, size, and weight if available
+// If size or weight is given transform the value to a string
+// Attach the unit cm to the size
+// Attach the unit kg to the weight
+// Return a new object with all available properties that we are interested in
+
+
+
+
+
+
+type ObjectT59 = Record<string | number, number | string>
+
+const task59 = (a: ObjectT59) => {
+
+    let res: ObjectT59 = {};
+
+    Object.keys(a).forEach(key => {
+
+        if(key === 'fn' || key === 'ln'){
+            res[key] = a[key];
+        }
+
+        if(key === 'size'){
+            a[key] = a[key] + 'cm';
+            res[key] = a[key];
+
+        }
+
+        if(key === 'weight'){
+            a[key] = a[key] + 'kg';
+            res[key] = a[key];
+        }
+        
+    });
+
+    return res;
+}
+
+console.log('TASK59')
+console.log(task59({fn: 'Lisa', ln: 'Müller', age: 17, size: 175, weight: 67}));
+console.log(task59(({fn: 'Martin', ln: 'Harper', age: 26, email: 'martin.harper@test.de', weight: 102})));
+console.log(task59({fn: 'Andrew', ln: 'Harper', age: 81, size: 175, weight: 71}));
+console.log(task59({fn: 'Matthew', ln: 'Müller', age: 19, email: 'matthew@mueller.de'}));
+
+
+// TASK60
+// Write a function that takes an array of objects and a string as arguments
+// Add a property with key 'continent' and value equal to the string to each of the objects
+// Return the new array of objects
+// Tip: try not to mutate the original array
+
+
+
+
+
+
+
+type ObjectT60 = {
+    [key: string | number]: [string | number];
+};
+
+const task60 = (a: ObjectT60,  c: string) => {
+
+    let res: ObjectT59 = {};
+
+    Object.keys(a).forEach(key => {
+
+        if(key === 'fn' || key === 'ln'){
+            res[key] = a[key];
+        }
+
+        if(key === 'size'){
+            a[key] = a[key] + 'cm';
+            res[key] = a[key];
+
+        }
+
+        if(key === 'weight'){
+            a[key] = a[key] + 'kg';
+            res[key] = a[key];
+        }
+        
+    });
+
+    return res;
+}
+
+console.log('TASK60')
+console.log(task60([{ city: 'Tokyo', country: 'Japan' }, { city: 'Bangkok', country: 'Thailand' }], 'Asia'));
+console.log(task60([{ city: 'Stockholm', country: 'Sweden' }, { city: 'Paris', country: 'France' }], 'Europe'));
+console.log(task60([{ city: 'Stockholm', country: 'Sweden' }, { city: 'Paris', country: 'France' }], 'Europe'));
+ 
